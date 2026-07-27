79063afe30c32ef321c83f9ae111fc3f4c1bf996
Aya Hamayel <palestinepalestine16@gmail.com>
2026-07-27 20:38:58 +0300

    chang schema

--- src/schemas/list-task.ts
+++ src/schemas/list-task.ts
@@ -1,4 +1,7 @@
 import { z } from "zod";
 
-export const listTasksInputSchema = z.object({});
+export const listTasksInputSchema = z.object({
+      text: z.string().min(1).describe("The task description"),
+
+});
 
